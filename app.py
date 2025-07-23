819e5d23f36c3a5a9ec761d14deff735d69c27e9
festus-sulumeti <sulumetifestus@outlook.com>
2025-07-23 14:54:01 +0300

    fix: update CORS settings to support credentials and allow all origins for frontend compatibility

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+
 
 
 # Hardcoded admin credentials
