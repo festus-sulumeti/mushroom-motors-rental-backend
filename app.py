dc4cbb61b9df9b2adb116f9489e8998c004aac63
festus-sulumeti <sulumetifestus@outlook.com>
2025-07-25 12:10:45 +0300

    feat(api): add GET /api/users endpoint to fetch all registered users

--- app.py
+++ app.py
@@ -84,6 +84,22 @@ def user_login():
         }), 200
     else:
         return jsonify({'success': False, 'message': 'Invalid credentials'}), 401
+    
+
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    user_list = [
+        {
+            'id': user.id,
+            'name': user.name,
+            'email': user.email,
+            'role': 'Admin' if user.is_admin else 'Customer'
+        }
+        for user in users
+    ]
+    return jsonify({'users': user_list}), 200
+
 
 
 # ---- Run App Locally ----
