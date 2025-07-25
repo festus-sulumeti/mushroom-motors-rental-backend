c4c6840b85804b47474a76b541a25dc23ae8e493
festus-sulumeti <sulumetifestus@outlook.com>
2025-07-25 14:30:39 +0300

    feat: added the Car mmodels table

--- models.py
+++ models.py
@@ -15,3 +15,15 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User(name='{self.name}', email='{self.email}', is_admin={self.is_admin})>"
+
+class Car(db.Model):
+    __tablename__ = 'cars'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    model = db.Column(db.String(50), nullable=False)
+    status = db.Column(db.String(20), nullable=False, default='Available')  # 'Available' or 'Rented'
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Car(name='{self.name}', model='{self.model}', status='{self.status}')>"
